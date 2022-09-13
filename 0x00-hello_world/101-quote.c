a4b84bf9e1a4272f9eed68b574b8eceecf4a332c
Jou <unahojude@gmail.com>
2022-09-12 17:01:30 -0700

    Adding files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - entry point
+ *
+ * return: always 1 (succes)
+ */
+
+int main (v)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
