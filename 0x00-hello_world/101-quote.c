7e0e1d41bde6a383db5a9e63a577f2fed3cadb32
Unah Jou <unahojude@gmail.com>
2022-12-09 12:31:44 -0800

    Fixed 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * Main - entry point
  * Description: 'To print a string accurately'
- * Return: always 1 (succes)
+ * Return: always 0 (succes)
  */
 
 int main(void)
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-return (1);
+return (0);
 }
