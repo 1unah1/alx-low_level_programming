6772d35329ed9d87e8cfbecef60170ffbbff58c3
1unah1 <unahojude@gmail.com>
2023-10-19 11:35:25 -0700

    Fixed main file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b);
 int strlen(char *s);
 void puts(char *str);
 void print_rev(char *s);
